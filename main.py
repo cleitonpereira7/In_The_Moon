91819f566f4272a876cfd36e9f8285aade174e69
Cleiton <cleitonpereira612@gmail.com>
2019-06-15 04:21:07 -0300

    Arquivos modificados e carregamento de imagens

--- main.py
+++ main.py
@@ -22,15 +22,36 @@ running = True
 
 # Load Images
 icon = pygame.image.load('_images/rocket.png')
+bg_initial = pygame.image.load('_images/gameover.png')
+
+walk_right = [pygame.image.load('_images/_player/player_d01.png'),
+              pygame.image.load('_images/_player/player_d02.png'),
+              pygame.image.load('_images/_player/player_d03.png')]
+
+walk_left = [pygame.image.load('_images/_player/player_e01.png'),
+              pygame.image.load('_images/_player/player_e02.png'),
+              pygame.image.load('_images/_player/player_e03.png')]
+
+walk_up = [pygame.image.load('_images/_player/player_c01.png'),
+              pygame.image.load('_images/_player/player_c02.png'),
+              pygame.image.load('_images/_player/player_c03.png')]
+
+walk_front = [pygame.image.load('_images/_player/player_f01.png'),
+              pygame.image.load('_images/_player/player_f02.png'),
+              pygame.image.load('_images/_player/player_f03.png')]
 
 # Title and Icon
 pygame.display.set_caption('In The Moon')
 pygame.display.set_icon(icon)
 
+# Main
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:                       # Ao clicar no botão X ele sai do jogo
             running = False
+        if event.type == pygame.KEYDOWN:                    # Ao apertar a tecla ESC ele também irá sair
+            if event.key == pygame.K_ESCAPE: 
+                running = False
 
-        screen.fill(colors[0])      
+        screen.blit(walk_right[1], [0, 0])                      # Seta o background no inicio da tela e do Jogo
         pygame.display.update()
